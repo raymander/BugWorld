9635ac16082d9f52b7b789642d21200c547ff94f
Ray Chern <ray.chern@fasolutions.com>
2019-01-29 11:05:04 +0200

    add foodController

---
+++ FoodController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodController : MonoBehaviour
+{
+    public GameObject[] flakes;
+    public float rotationSpeed; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationSpeed = 50;
+
+        //needs to be changed when flakes are set to spawn automatically
+        flakes = GameObject.FindGameObjectsWithTag("Food");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject flake in flakes)
+        {
+            flake.transform.Rotate(0, rotationSpeed*Time.deltaTime, 0);
+        }
+    }
+}
